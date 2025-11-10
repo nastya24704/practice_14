f3f05dfdaac4f304575f23e937015bbfb292ceaf
nastya24704 <nastazeravina@gmail.com>
2025-11-11 02:00:38 +0700

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,1 +1,22 @@
+def main() -> None:
+    """
+    Reads a list of integers from user input, calculates
+    the sum of even and odd numbers, and prints the results.
+    """
+    elements = input("Enter integer numbers separated by spaces: ")
+    numbers = [int(num) for num in elements.split()]
 
+    sum_even = 0
+    sum_odd = 0
+
+    for number in numbers:
+        if number % 2 == 0:
+            sum_even += number
+        else:
+            sum_odd += number
+
+    print(sum_even, sum_odd)
+
+
+if __name__ == "__main__":
+    main()
