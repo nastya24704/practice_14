658c46b68e06930f9b4ccfdee0b016100f74a3bc
nastya24704 <nastazeravina@gmail.com>
2025-11-11 01:21:16 +0700

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,12 @@
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+
+punctuation_marks = '.,?!...:;,-"()/'
+result = []
+
+for word in words:
+    final_word = word.strip(punctuation_marks)
+    if final_word not in result:
+        result.append(final_word)
+
+print(result)
