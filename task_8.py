f55c12a1e8fe81366e0b1e6439f354b762bf89cd
nastya24704 <nastazeravina@gmail.com>
2025-11-11 10:55:51 +0700

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,1 +1,28 @@
+def sort_string(user_str: str) -> str:
+    """
+    Sort characters in a string in ascending order.
 
+    Args:
+        user_string (str): The input string to be sorted
+
+    Returns:
+        str: A new string with characters sorted in ascending order
+
+    """
+    elements = list(user_str)
+    elements.sort()
+    sorted_elements = ''.join(elements)
+    return sorted_elements
+
+
+def main():
+    """
+    Main function to get user input and display sorted string.
+    """
+    user_str = input("Enter the string: ")
+    sorted_str = sort_string(user_str)
+    print("Sorted string:", sorted_str)
+
+
+if __name__ == "__main__":
+    main()
