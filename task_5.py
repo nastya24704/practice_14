ad4c1129a4d34974c32b5064ec5a97bc4e02c282
nastya24704 <nastazeravina@gmail.com>
2025-11-11 10:59:50 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,4 +1,5 @@
-elements = input("Enter integer numbers separated by spaces: ")
-numbers = [int(num) for num in elements.split()]
-average = sum(numbers) / len(numbers)
-print(average)
+def main() -> None:
+  elements = input("Enter integer numbers separated by spaces: ")
+  numbers = [int(num) for num in elements.split()]
+  average = sum(numbers) / len(numbers)
+  print(average)
